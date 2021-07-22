341e186e53902fe63383c9f28296d7411b8639d7
drewsview34 <drewsview34@outlook.com>
2021-07-22 13:47:29 -0700

    created forms for the new and edit actions for both models. Dispalys errors for invalid actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,15 +12,27 @@ class PostsController < ApplicationController
   end
 
   def create
-    
+    @post = Post.new(post_params)
+
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+
   end
 
   def update
-    @post = Post.find(params[:id])
-
+    @post = Post.find(params[:id])  
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.save 
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+
   end
 
   private
